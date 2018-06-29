647419dd6af237f09d110bcd0d53c4270b51a953
4d11 <aadilasim@hotmail.com>
2018-06-28 22:16:41 -0400

    Output formatted file

--- sooFresh.py
+++ sooFresh.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup, Tag
+import os
 
-bulmaCDN = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+file_path = 'test/test1.html'
 
-f = open("test/test1.html")
+f = open(file_path)
 f_data = f.read()
 
 soup = BeautifulSoup(f_data, 'html.parser')
+
+
+class SooFresh():
+    def __init__(self, html, framework):
+        self.html = html
+        self.framework = framework
+
+
+
 
 def get_bulma_starter():
     # hack since name is reserved
@@ -19,15 +29,26 @@ def get_bulma_starter():
     return meta, stylesheet
 
 
+def initialize_framework(head: Tag):
+    meta, stylesheet = get_bulma_starter()
+
+    head.append(meta)
+    head.append(stylesheet)
+
+
+
+initialize_framework(soup.head)
+
+
 for btn in soup.find_all('button'):
     print(btn.get('class'))
 
-meta, stylesheet = get_bulma_starter()
+# meta, stylesheet = get_bulma_starter()
 
-soup.head.append(meta)
-soup.head.append(stylesheet)
+# soup.head.append(meta)
+# soup.head.append(stylesheet)
 
-print(soup.head)
+# print(soup.head)
 
 
 
@@ -39,6 +60,15 @@ def import_framework(name):
     if (name == 'bulma'):
         meta, stylesheet = get_bulma_starter()
 
+
+
+folder = os.path.dirname(file_path)
+file = os.path.basename(file_path)
+file_name, ext = os.path.splitext(file)
+
+new_file_name = os.path.join(folder, file_name+'-fresh'+ext)
+with open(new_file_name, 'w') as out_file:
+    out_file.write(soup.prettify())
 
 
 
