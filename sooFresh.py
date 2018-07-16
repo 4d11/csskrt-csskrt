a73ce19928c92111893d714a57ddb3afeace2f6c
4d11 <aadilasim@hotmail.com>
2018-07-15 21:42:14 -0400

    Implemented support for form element wrappers

--- sooFresh.py
+++ sooFresh.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup, Tag, NavigableString
 import os
 
 file_path = 'test/test1.html'
@@ -8,6 +8,16 @@ f_data = f.read()
 
 soup = BeautifulSoup(f_data, 'html.parser')
 
+def add_class_to_element(elem, css_class):
+    if not elem.get('class'):
+        elem['class'] = css_class
+    else:
+        try:
+            elem['class'].append(css_class)
+        except AttributeError:
+            elem['class'] += ' ' + css_class
+
+
 
 class SooFresh():
     def __init__(self, html, framework):
@@ -16,7 +26,7 @@ class SooFresh():
 
 
 def get_bulma_starter():
-    # hack since name is reserved
+    # hack since 'name' is reserved
     meta = Tag(builder=soup.builder,
                name='meta',
                attrs={'name': "viewport", 'content': 'width=device-width, initial-scale=1'})
@@ -36,21 +46,56 @@ def initialize_framework(head: Tag):
 
 def apply_tag_styles(tag_dict: dict, soup):
     for tag, css_class in tag_dict.items():
-        if tag == 'radio' or tag == 'checkbox':
-            continue
         for elem in soup.find_all(tag):
-            if not elem.get('class'):
-                elem['class'] = css_class
-            else:
-                elem['class'].append(css_class)
+            add_class_to_element(elem, css_class)
 
-            if tag == 'input':
+
+def add_form_classes(soup, tag_dict: dict):
+    for form in soup.find_all('form'):
+        form_contents = form.contents
+        spotted_label = None
+        for elem in form.children:
+            if elem.name == 'label':
+                spotted_label = elem
+                add_class_to_element(elem, 'label')
+            elif elem.name == 'input':
+                add_class_to_element(elem, 'input')
+
                 if elem.get('type') == 'radio':
                     if 'radio' in tag_dict:
-                        elem['class'] = elem['class'] + " " + tag_dict['radio']
+                        add_class_to_element(elem, tag_dict['radio'])
                 elif elem.get('type') == 'checkbox':
                     if 'checkbox' in tag_dict:
-                        elem['class'] = elem['class'] + " " + tag_dict['checkbox']
+                        add_class_to_element(elem, tag_dict['checkbox'])
+
+                # Add overall wrapper
+                if not spotted_label:
+                    # add wrapper
+                    field_div = soup.new_tag('div', **{'class': 'field'})
+                    elem.wrap(field_div)
+                else:
+                    # the input has a preceding label
+                    field_div = soup.new_tag('div', **{'class': 'field'})
+                    spotted_label.wrap(field_div)
+                    field_div.append(elem)
+                    spotted_label = None
+
+                 # Add input wrapper
+                control_div = soup.new_tag('div', **{'class': 'control'})
+                elem.wrap(control_div)
+            elif type(elem) == Tag: # ignore  NavigableStrings like /n
+                spotted_label = None
+                if (tag_dict.get(elem.name)):
+                    add_class_to_element(elem, tag_dict[elem.name])
+
+
+def freshify(soup):
+    initialize_framework(soup.head)
+    # apply_tag_styles(tag_styles, soup)
+    add_form_classes(soup, tag_styles)
+
+
+
 
 
 tag_styles = {
@@ -63,20 +108,7 @@ tag_styles = {
     'radio': 'radio',
 }
 
-# input styles
-# 'checkbox': 'checkbox',
-# 'radio': 'radio',
-
-
-initialize_framework(soup.head)
-
-
-def import_framework(name):
-    if (name == 'bulma'):
-        meta, stylesheet = get_bulma_starter()
-
-
-apply_tag_styles(tag_styles, soup)
+freshify(soup)
 
 folder = os.path.dirname(file_path)
 file = os.path.basename(file_path)
