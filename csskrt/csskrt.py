8dd2b66521702fa13fe439f51cd03d16a7228159
4d11 <aadilasim@hotmail.com>
2018-08-05 23:19:06 -0400

    Added output directory to tests

--- csskrt/csskrt.py
+++ csskrt/csskrt.py
@@ -172,7 +172,7 @@ class Csskrt(ABC):
         file = os.path.basename(self.file_path)
         file_name, ext = os.path.splitext(file)
 
-        new_file_name = os.path.join(folder, 'csskrt_' + file_name + ext)
+        new_file_name = os.path.join(folder, 'output/csskrt_' + file_name + ext)
         with open(new_file_name, 'w') as out_file:
             if self.pretty_print:
                 out_file.write(str(self.soup))
